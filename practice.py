5a44ce4d23ee31a1d42e11c5876cfb29c5dbd2b1
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 14:10:51 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,8 @@
-print("hello")
-print("world")
-print("hello")
+def mystery_function(values):
+    result = []
+    for sublist in values:
+      result.append([sublist[0]])
+      for i in sublist[1:]:
+        result[-1].insert(0,i)
+        
+    return result
